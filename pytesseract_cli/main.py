8109a8e93c6aa463f824a22dda17b76cc8569fe5
Braden Dubois <braden.dubois@usask.ca>
2021-05-14 13:34:29 -0600

    feat: cli now enabled; run 'pytesseract-cli'

--- pytesseract_cli/main.py
+++ pytesseract_cli/main.py
@@ -1,8 +1,10 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from PIL import Image, UnidentifiedImageError
-from pytesseract import image_to_string, image_to_pdf_or_hocr
+from pytesseract import get_languages, image_to_string, image_to_pdf_or_hocr
+from sys import argv
 from typing import Union
+
 
 parse_functions = {
     "pdf": image_to_pdf_or_hocr,
@@ -10,59 +12,66 @@ parse_functions = {
 }
 
 
-def write_parsed(contents: Union[bytes, str], destination):
+def write_parsed(contents: Union[bytes, str], destination: Path):
+    """
+    Write a parsed / interpreted contents of an image to some destination path
+    :param contents: Parsed contents after running a pytesseract OCR function
+    :param destination: A Path to write the contents to
+    """
     print(destination)
     with destination.open(f"w{'+b' if isinstance(contents, bytes) else ''}") as f:
         f.write(contents)
 
 
-def process_file(file: Path):
+def process_file(file: Path, namespace: Namespace):
+    """
+    Parse a file using pytesseract
+    :param file: A Path to some file that can be opened as a PIL.Image
+    :param namespace: namespace of parameters parsed from command-line input
+    """
 
     assert file.is_file()
 
     try:
         image = Image.open(file)
     except UnidentifiedImageError:
-        print(f"File: {file.absolute()} cannot be opened as a file : skipping")
+        print(f"file: {file.absolute()} cannot be opened as a file : skipping")
         return
 
     # TODO - Include language
-    result = parse_functions[parser.file_type](image, lang=parser.lang)
+    result = parse_functions[namespace.file_type](image, lang=namespace.lang)
 
     # if parser.join:
     #     parsing_buffer.append(result)
     # else:
-    resulting_path = file.with_suffix(f".{parser.file_type}")
+    resulting_path = file.with_suffix(f".{namespace.file_type}")
     write_parsed(result, resulting_path)
 
 
-def process_directory(directory: Path):
+def process_directory(directory: Path, namespace: Namespace):
+    """
+    Process all valid files in a given directory.
+    :param directory: A Path to some directory
+    :param namespace: namespace of parameters parsed from command-line input
+    """
 
     assert directory.is_dir()
 
     dir_list = sorted(directory.iterdir())
 
     for file in filter(lambda entry: entry.is_file(), dir_list):
-        process_file(file)
+        process_file(file, namespace)
 
-    if parser.recurse:
+    if namespace.recurse:
         for nested_directory in filter(lambda entry: entry.is_dir(), dir_list):
-            process_directory(nested_directory)
+            process_directory(nested_directory, namespace)
 
 
 def run():
-
-    for file in parser.files:
-        process_file(Path(file))
-
-    for directory in parser.directories:
-        process_directory(Path(directory))
-
-    # if parser.join:
-    #     ...
-
-
-if __name__ == "__main__":
+    """
+    "Main" of a file that interprets all command-line arguments and processes any files or
+    directories accordingly.
+    """
 
     parsing_buffer = []
 
@@ -72,7 +81,7 @@ if __name__ == "__main__":
     parser.add_argument('-f', dest="files", nargs='*', default=[], type=str, help="name(s) of file(s) to process")
     parser.add_argument('-d', dest="directories", nargs='*', default=[], type=str, help="directory(s) to process")
 
-    parser.add_argument('-r', dest="recurse", action="store_const", const=True, default=True,
+    parser.add_argument('-r', dest="recurse", action="store_const", const=True, default=False,
                         help="recurse on all directories listed")
 
     # parser.add_argument('-j', dest="join", required=False, type=str,
@@ -83,9 +92,28 @@ if __name__ == "__main__":
 
     parser.add_argument('-l', dest="lang", type=str, help="language of the text in any image(s)", required=False)
 
-    parser = parser.parse_args()
+    parser.add_argument('--list-languages', dest="list", action="store_const", const=True, default=True,
+                        help="list all languages available")
 
-    run()
+    if len(argv) < 2:
+        parser.print_help()
+        exit(0)
+
+    namespace = parser.parse_args()
+
+    if namespace.list:
+        print("languages available:\n-", "\n- ".join(get_languages()))
+        exit(0)
+
+    for file in namespace.files:
+        process_file(Path(file), namespace)
+
+    for directory in namespace.directories:
+        process_directory(Path(directory), namespace)
 
     # if parser.join:
     #     ...
+
+
+if __name__ == "__main__":
+    run()
